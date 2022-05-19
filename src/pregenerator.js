a053ff17f86fe838d1425213217865bbd937e6bc
Yudong Zhu <zhuyudong@aliyun.com>
2022-05-19 15:16:56 +0800

    fix: 🐛 项目根目录实际路径判断

--- src/pregenerator.js
+++ src/pregenerator.js
@@ -1,7 +1,7 @@
 const { cwd } = require('./root')
-const chalk = require('chalk')
+// const chalk = require('chalk')
 
-console.log(chalk.gray(`$PWD: ${cwd}`))
+// console.log(chalk.gray(`$PWD: ${cwd}`))
 if (!cwd.endsWith('/kunlun-fabric')) {
   // @ts-ignore
   require('./generator')
--- src/root.js
+++ src/root.js
@@ -2,11 +2,19 @@ const { resolve } = require('path')
 
 let cwd = process.env.INIT_CWD
 // 在 node_modules/@kunlunjs/fabric/src 中
-if (!cwd && process.cwd() === resolve(__dirname, '..')) {
-  cwd = resolve(__dirname, '../../../..')
-}
+// if (!cwd && process.cwd() === resolve(__dirname, '..')) {
+//   cwd = resolve(__dirname, '../../../..')
+// }
 if (process.env.PWD.endsWith('@kunlunjs/fabric')) {
   cwd = resolve(__dirname, '../../..')
+}
+if (
+  !cwd &&
+  /node_modules/.test(__dirname) &&
+  __dirname.split('node_modules').length
+) {
+  // '<path>/node_modules/.pnpm/@kunlunjs+fabric@0.2.9/node_modules/package.json' -> <path>
+  cwd = __dirname.split('node_modules')[0].slice(0, -1)
 }
 // 在 kunlun-fabric 中
 if (!cwd && process.cwd() === __dirname) {

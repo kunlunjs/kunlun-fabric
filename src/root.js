6d8e943e55bceecc1bb117e937993e9f7768c8e6
Yudong Zhu <zhuyudong@aliyun.com>
2022-05-19 15:26:17 +0800

    fix: 🐛 项目根目录实际路径判断

--- src/root.js
+++ src/root.js
@@ -8,20 +8,16 @@ let cwd = process.env.INIT_CWD
 if (process.env.PWD.endsWith('@kunlunjs/fabric')) {
   cwd = resolve(__dirname, '../../..')
 }
-if (
-  !cwd &&
-  /node_modules/.test(__dirname) &&
-  __dirname.split('node_modules').length
-) {
-  // '<path>/node_modules/.pnpm/@kunlunjs+fabric@0.2.9/node_modules/package.json' -> <path>
-  cwd = __dirname.split('node_modules')[0].slice(0, -1)
-}
 // 在 kunlun-fabric 中
 if (!cwd && process.cwd() === __dirname) {
   cwd = process.cwd()
 }
 if (!cwd) {
   cwd = process.cwd()
+}
+if (/node_modules/.test(cwd) && cwd.split('node_modules').length) {
+  // '<path>/node_modules/.pnpm/@kunlunjs+fabric@0.2.9/node_modules/package.json' -> <path>
+  cwd = __dirname.split('node_modules')[0].slice(0, -1)
 }
 
 const pkg = require(resolve(cwd, 'package.json'))
